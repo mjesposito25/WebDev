2fdaad944c0742122c37c4c7a6d60a08241d430a
mjesposito25 <matt.joseph.esposito@gmail.com>
2022-07-26 09:49:41 -0400

    Login App

---
+++ WebDevTutorials/LoginAndRegisterApp/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using LoginAndRegisterApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginAndRegisterApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if (userModel.UserName == "Bill" && userModel.Password == "bigbucks")
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else
+            {
+                return View("LoginFailure", userModel);
+            }
+            
+        }
+    }
+}
