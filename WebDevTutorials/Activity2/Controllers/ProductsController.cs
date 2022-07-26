dbac6d524a48d92961463819381913a33843fc1e
mjesposito25 <matt.joseph.esposito@gmail.com>
2022-07-26 09:19:07 -0400

    Activity2 App

---
+++ WebDevTutorials/Activity2/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Activity2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int secretNumber=4)
+        {
+            ViewBag.Name = name;
+            ViewBag.Secret = secretNumber;
+            return View();
+        }
+    }
+}
